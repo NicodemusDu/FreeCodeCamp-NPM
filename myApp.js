e67bed1a162f65888217d976e96bdbc4d639f9c3
Nicodemus <100146335+NicodemusDu@users.noreply.github.com>
2022-05-06 13:02:21 +0000

    Basic Node and Express - 08

--- myApp.js
+++ myApp.js
@@ -4,6 +4,13 @@ let app = express();
 
 app.use('/public', express.static(__dirname+'/public'));
 
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString();
+  next();
+}, function(req, res){
+  res.send({'time': req.time})
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/views/index.html');
   console.log('Hello Express');
@@ -17,6 +24,11 @@ app.get('/json', function(req, res){
     msg = msg.toUpperCase();
   }
   res.send({"message": msg});
+});
+
+app.get('/:word/echo', function(req, res, next){
+  console.log('parameter word:\t', req.params.word);
+  res.send({'echo: ': req.params.word});
 });
 
 module.exports = app;
